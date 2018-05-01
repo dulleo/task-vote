15aaf7dd04149bd56ece357912762d7e91f3f316
duskol <dulleo@gmail.com>
2018-05-01 13:26:15 +0200

    Load 3 tasks

--- TaskVoteApp/src/main/java/com/example/demo/dataloader/DataLoader.java
+++ TaskVoteApp/src/main/java/com/example/demo/dataloader/DataLoader.java
@@ -1,5 +1,8 @@
 package com.example.demo.dataloader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -23,13 +26,14 @@ public class DataLoader implements CommandLineRunner {
 		logger.info("Delete old data from db started.");
 		taskRepository.deleteAll().block();
 		logger.info("Delete old data from db completed...");
-		
-		Task t = new Task("Task1", 0, 0);
+	
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task("Task1", 1, 1));
+		tasks.add(new Task("Task2", 2, 2));
+		tasks.add(new Task("Task3", 3, 3));
 		
 		logger.info("Loading data started");
-		taskRepository.save(t).block();
-		logger.info("Loading data completed...");
-		
+		taskRepository.saveAll(tasks).blockLast();
+		logger.info("Loading data completed...");	
 	}
-
 }
