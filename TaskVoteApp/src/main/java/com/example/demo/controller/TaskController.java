a63106842825f612ddc8e5bcb205ae26d92df378
duskol <dulleo@gmail.com>
2018-05-01 13:59:07 +0200

    create task and delete task added

--- TaskVoteApp/src/main/java/com/example/demo/controller/TaskController.java
+++ TaskVoteApp/src/main/java/com/example/demo/controller/TaskController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +16,7 @@ import com.example.demo.model.Task;
 import com.example.demo.service.TaskService;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @CrossOrigin(origins= "http://localhost:4200")
 @RestController
@@ -22,5 +29,15 @@ public class TaskController {
 	@GetMapping(value = "/tasks")
 	public Flux<Task> getAllTasks() {
 		return taskService.getAllTasks();
+	}
+	
+	@PostMapping("/tasks")
+	public Mono<Task> createTask(@Valid @RequestBody Task task) {
+		return taskService.createTask(task);
+	}
+	
+	@DeleteMapping("/tasks/{id}")
+	public void deleteTask(@PathVariable String id) {
+		taskService.deleteTask(id);
 	}
 }
