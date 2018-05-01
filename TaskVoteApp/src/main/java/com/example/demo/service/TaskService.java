7f063384b270d50de8960d4ad39d41ea0b31b748
duskol <dulleo@gmail.com>
2018-05-01 13:58:47 +0200

    Create and Delete method added

--- TaskVoteApp/src/main/java/com/example/demo/service/TaskService.java
+++ TaskVoteApp/src/main/java/com/example/demo/service/TaskService.java
@@ -3,9 +3,14 @@ package com.example.demo.service;
 import com.example.demo.model.Task;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface TaskService {
 	
 	Flux<Task> getAllTasks();
+
+	Mono<Task> createTask(Task task);
+
+	void deleteTask(String id);
 	
 }
