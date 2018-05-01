179d4a47ae7cfb71816ef5831505b15a91fdc58b
duskol <dulleo@gmail.com>
2018-05-01 13:00:59 +0200

    @Service annotation added

--- TaskVoteApp/src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ TaskVoteApp/src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Task;
 import com.example.demo.repository.TaskRepository;
 
 import reactor.core.publisher.Flux;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
 	@Autowired
