9f4c21d44f10ea12e0f87c9c9f4e1f02384c5dbb
duskol <dulleo@gmail.com>
2018-05-05 08:01:01 +0200

    vote endpoint added

--- TaskVoteApp/src/main/java/com/example/demo/controller/TaskController.java
+++ TaskVoteApp/src/main/java/com/example/demo/controller/TaskController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.TaskVoteDTO;
 import com.example.demo.model.Task;
 import com.example.demo.service.TaskService;
 
@@ -39,5 +41,16 @@ public class TaskController {
 	@DeleteMapping("/tasks/{id}")
 	public void deleteTask(@PathVariable String id) {
 		taskService.deleteTask(id);
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param taskVoteDTO
+	 * @return
+	 */
+	@PutMapping("/tasks/vote/{id}")
+	public Mono<Task> vote(@PathVariable("id") String id, @Valid @RequestBody TaskVoteDTO taskVoteDTO) {
+		return taskService.vote(taskVoteDTO);
 	}
 }
