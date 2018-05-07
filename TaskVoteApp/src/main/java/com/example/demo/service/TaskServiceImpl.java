2590b1c67b923014f4ae7897cce36564876e7bb3
duskol <dulleo@gmail.com>
2018-05-07 13:01:10 +0200

    log added

--- TaskVoteApp/src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ TaskVoteApp/src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.example.demo.dto.TaskVoteDTO;
 import com.example.demo.model.Task;
@@ -13,13 +16,15 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+	
+	private final Logger logger = LoggerFactory.getLogger(TaskServiceImpl.class);
 
 	@Autowired
 	private TaskRepository taskRepository;
 	
 	@Override
 	public Flux<Task> getAllTasks() {
-		
+		logger.info("findAll() tasks...");
 		return taskRepository.findAll();
 		
 	}
